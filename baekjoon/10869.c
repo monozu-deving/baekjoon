e92bc4215f0544a5b7bdedaa1cd01209f7a48818
monozu-deving <monozu.deving@gmail.com>
2025-05-27 02:00:01 +0900

    number 10869 solve

---
+++ baekjoon/10869.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d\n", a % b);
+	return 0;
+}
